16f3baa3bfde1d64a1888899fec94d2079399fe9
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-04-17 18:21:14 +0200

    add \beta

--- src/HopfieldRBM.jl
+++ src/HopfieldRBM.jl
@@ -5,21 +5,26 @@ struct RBM{T}
     ξ::AbstractMatrix{T}
     m::AbstractVector{T}
     σ::AbstractVector{T}
-    function RBM{T}(N,P) where T
+    function RBM{T}(N::Integer,P::Integer) where T
         ξ=ones(T,N,P)
         m=ones(T,P)
         σ=ones(T,N)
         new{T}(N,P,ξ,m,σ)
     end
-    RBM(N,P)=RBM{Float64}(N,P)
-    function RBM(ξ::AbstractMatrix{T}) where T
+    RBM(N::Integer,P::Integer)=RBM{Float64}(N,P)
+    function RBM(ξ::AbstractMatrix{T},σ::AbstractVector{T}) where T
         N,P=size(ξ)
-        m=similar(ξ,P)
-        σ=similar(ξ,N)
-        σ.=1
+        N==length(σ) || error("σ must be long $N, not ",length(σ),".")
+        m=similar(σ,P)
         S=new{T}(N,P,ξ,m,σ)
         refresh_m!(S)
         S
+    end
+    function RBM(ξ::AbstractMatrix{T}) where T
+        N,P=size(ξ)
+        σ=similar(ξ,N)
+        σ.=1
+        RBM(ξ,σ)
     end
 end
 
@@ -49,12 +54,12 @@ function flip_σ!(S::RBM{T},k) where T
     S.σ[k]=-sign(S.σ[k])
 end
 
-function glauber_flip_σ!(S::RBM{T}) where T
+function glauber_flip_σ!(S::RBM{T},β::T) where T
     H=energy(S)
     for i in 1:S.N
         flip_σ!(S,i)
         fH=energy(S)
-        p=1 / (1 + exp(H - fH))
+        p=1 / (1 + exp(β*(H - fH)))
         if rand() < p
             H=fH
         else
@@ -69,7 +74,7 @@ function ∇logP!(S::RBM{T},g::AbstractMatrix{T},σD::AbstractVector{T}) where T
     end
     NC=5
     for i in 1:NC
-        glauber_flip_σ!(S)
+        glauber_flip_σ!(S,one(T))
         for μ in 1:S.P, k in 1:S.N
             g[k,μ]-=S.m[μ]*S.σ[k]/NC
         end
@@ -78,28 +83,35 @@ end
 using PyPlot
 
 function testfigure()
-    S=RBM(rand((-1.0,1.0),100,2).*0.01)
+    S=RBM(rand((-1.0,1.0),100,3).*0.1)
     pastξ=copy(S.ξ)
     s1=ones(100);
     s1[1:50].*=-1;
     s2=copy(s1);
     s2[1:25].*=-1;
     s2[75:100].*=-1;
-    g=zeros(100,2);
+    s3=copy(s2);
+    s3[1:12].*=-1;
+    s3[37:50].*=-1;
+    s3[51:100].=s3[1:50]
+    g=similar(S.ξ);
 
-    for  i in 1:40
+    for  i in 1:50
         g.=0
         ∇logP!(S,g,s1)
         ∇logP!(S,g,s2)
+        ∇logP!(S,g,s3)
         S.ξ .+= 0.05 .* g
     end
-    S.ξ
 
     pygui(true)
     subplot(1,2,1)
     imshow(pastξ, interpolation="nearest", aspect="auto")
+    colorbar()
     subplot(1,2,2)
     imshow(S.ξ, interpolation="nearest", aspect="auto")
+    colorbar()
+    tight_layout()
 end
 
 testfigure()
